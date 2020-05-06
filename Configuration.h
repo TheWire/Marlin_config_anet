e87d56ccaa22a736c74c3ec426b97c111ea980a5
Andrew Barber <andrew@barber-net.co.uk>
2020-05-06 23:34:31 +0100

    disable MESH_EDIT_GFX_OVERLAY
    doesn't work with anet lcd

--- Configuration.h
+++ Configuration.h
@@ -1141,8 +1141,7 @@
   //===========================================================================
   //========================= Unified Bed Leveling ============================
   //===========================================================================
-  //** enabled with UBL
-  #define MESH_EDIT_GFX_OVERLAY   // Display a graphics overlay while editing the mesh
+  //#define MESH_EDIT_GFX_OVERLAY   // Display a graphics overlay while editing the mesh
   //** changed from 1 to 5
   #define MESH_INSET 1              // Set Mesh bounds as an inset region of the bed
   #define GRID_MAX_POINTS_X 10      // Don't use more than 15 points per axis, implementation limited.
