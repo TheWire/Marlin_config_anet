4f651f361a600281e5306a8c8d1dbfebcc1532b1
TheWire <andrew.ab@gmail.com>
2020-05-07 12:24:50 +0100

    reduced number of probe points
    10 reduced to 4 due to lack of rotary encoder, 10 too many to handle

--- Configuration.h
+++ Configuration.h
@@ -1144,7 +1144,8 @@
   //#define MESH_EDIT_GFX_OVERLAY   // Display a graphics overlay while editing the mesh
   //** changed from 1 to 5
   #define MESH_INSET 1              // Set Mesh bounds as an inset region of the bed
-  #define GRID_MAX_POINTS_X 10      // Don't use more than 15 points per axis, implementation limited.
+  //** changed from 10 to 4. 10 to many to handle without rotary switch
+  #define GRID_MAX_POINTS_X 4      // Don't use more than 15 points per axis, implementation limited.
   #define GRID_MAX_POINTS_Y GRID_MAX_POINTS_X
 
   #define UBL_MESH_EDIT_MOVES_Z     // Sophisticated users prefer no movement of nozzle
