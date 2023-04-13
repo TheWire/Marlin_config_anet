5794499f638b2b459209d9fab70e7aa19132e1ea
TheWire <TheWire@users.noreply.github.com>
2023-04-13 23:36:27 +0100

    extruder direction inverted

--- Marlin 2.1.3/Configuration.h
+++ Marlin 2.1.3/Configuration.h
@@ -1732,7 +1732,8 @@
 // @section extruder
 
 // For direct drive extruder v9 set to true, for geared extruder set to false.
-#define INVERT_E0_DIR false
+//** invert extruder for titan
+#define INVERT_E0_DIR true
 #define INVERT_E1_DIR false
 #define INVERT_E2_DIR false
 #define INVERT_E3_DIR false
